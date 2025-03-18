89bde8a2ba3490b4493c3cccd0b662399847c799
agathemczk <agathe.maciaszczyk@icloud.com>
2025-03-18 10:18:20 +0100

    coding tests

--- src/main/java/com/am/Collection2D.java
+++ src/main/java/com/am/Collection2D.java
@@ -5,4 +5,26 @@ import java.util.HashMap;
 
 public class Collection2D <E extends Collection2DElement<E>> extends HashMap<Point, List<E>> {
 
+    public void notifyElementHasMoved(final E collection2DElementTest, final Point oldPosition) {
+        
+    }
+
+    public void add(final E element) {
+    }
+
+    public void remove(final E element) {
+    }
+
+
+    public boolean contains(final E elementTest) {
+        return false;
+    }
+
+    public List<E> toList() {
+        return null;
+    }
+
+    public Dimension getDimension() {
+        return null;
+    }
 }
